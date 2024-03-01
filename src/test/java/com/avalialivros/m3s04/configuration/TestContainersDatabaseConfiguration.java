72d610ea62053719fd9b616552237a8dfe992e79
Marcelo Junqueira <marcelojxt@gmail.com>
2024-03-01 14:24:48 -0300

    [M03S05] Ex 7 - Mini-Projeto Avaliações - Testes de integração para o controlador de livros

--- src/test/java/com/avalialivros/m3s04/configuration/TestContainersDatabaseConfiguration.java
+++ src/test/java/com/avalialivros/m3s04/configuration/TestContainersDatabaseConfiguration.java
@@ -1,27 +1,24 @@
 package com.avalialivros.m3s04.configuration;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.lifecycle.Startables;
 
 import java.util.Map;
-import java.util.stream.Stream;
 
 @ContextConfiguration(initializers = TestContainersDatabaseConfiguration.Initializer.class)
 public class TestContainersDatabaseConfiguration {
 
     static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-        private static final Logger LOGGER = LoggerFactory.getLogger(Initializer.class);
         static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:16.1");
 
         private static void initContainer() {
-            Startables.deepStart(Stream.of(postgreSQLContainer)).join();
+            postgreSQLContainer
+                    .withInitScript("scripts/integrationtestinitscript.sql")
+                    .start();
         }
 
         private static Map<String, Object> createConnectionConfiguration() {
